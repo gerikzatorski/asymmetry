4f6835468a0a5cb7c8304e0f707409022033d51d
Gerik Zatorski <gerikzatorski@gmail.com>
2017-10-18 16:49:21 -0500

    realized I havedn't added the matplotlib testing side

---
+++ tests/AsymDAC/main.cpp
@@ -0,0 +1,19 @@
+// #include "Waveform.h"
+// #include <iostream>
+
+// using namespace std;
+
+// int main() 
+// {
+//   asymmetry::ATrianglewave wave;
+//   wave.init(1);
+//   wave.compute();
+
+//   cout << wave.approx(0.9) << endl;
+//   cout << wave.approx(1.9) << endl;
+  
+//   // wave.graph();
+//   // wave.graphApprox(100);
+  
+//   return 0;
+// } 
